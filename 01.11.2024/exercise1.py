289b741422f10e2aaf76f5d6c3e2940854fcb717
Daniyal Ganyatov <deni12349075@gmail.com>
2024-11-01 14:25:58 +0300

    added and created homework lab 6

---
+++ 01.11.2024/exercise1.py
@@ -0,0 +1,24 @@
+class UserAccount:
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.__password = password  
+    
+    def set_password(self, new_password):
+        
+        self.__password = new_password
+    
+    def check_password(self, password):
+        
+        return self.__password == password
+
+
+user = UserAccount("deni", "deni@mail.ru", "anypassword")
+
+
+user.set_password("new_secure_password")
+
+if(user.check_password):
+    print(user.check_password("new_secure_password"))  
+else:
+    print(user.check_password("wrong_password"))       
